d8523371f797fc9d93057bc4c18be6340ae4e357
Shimadakunn <lcombaret@hotmail.co.jp>
2025-02-14 23:44:49 +0100

    vercel deploy

--- src/config/PublicClient.ts
+++ src/config/PublicClient.ts
@@ -4,7 +4,7 @@ import { arbitrumSepolia } from "viem/chains";
 const client = createPublicClient({
   chain: arbitrumSepolia,
   transport: http(
-    "https://arbitrum-sepolia.infura.io/v3/a78ea67f650a46e8bd97f3262d1cef43"
+    `https://arbitrum-sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`
   ),
 });
 
