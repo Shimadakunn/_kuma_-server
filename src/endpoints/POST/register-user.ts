d3d36e98cd46cdc6a12de66791252ab6eb6500c0
Shimadakunn <89693356+Shimadakunn@users.noreply.github.com>
2025-02-26 23:26:17 +0100

    register user

--- src/endpoints/POST/register-user.ts
+++ src/endpoints/POST/register-user.ts
@@ -4,11 +4,26 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 const prisma = new PrismaClient().$extends(withAccelerate());
 
 export async function registerUser(address: string, email: string) {
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      OR: [{ address }, { email }],
+    },
+  });
+
+  if (existingUser) {
+    return prisma.user.update({
+      where: { id: existingUser.id },
+      data: { lastConnectedAt: new Date() },
+    });
+  }
+
   const user = await prisma.user.create({
     data: {
       address,
       email,
+      lastConnectedAt: new Date(),
     },
   });
+
   return user;
 }
